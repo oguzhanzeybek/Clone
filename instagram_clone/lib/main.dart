b3045125e8d8dc76cb9ec448a820e67d06cdcce0
oguzhanzeybek <darkstroge@gmail.com>
2022-12-18 05:18:49 +0300

    başlıyoruz

---
+++ instagram_clone/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'instagram',
+      theme: ThemeData.dark(),
+      
+      
+        
+      
+      home: Scaffold(body: Text("daha iyisi için şimdi."),)
+      
+    );
+  }
+}
+
+

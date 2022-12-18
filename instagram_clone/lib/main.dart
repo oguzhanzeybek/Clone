2496bad5177bce4ad72a7d9a74ec85e9bd0385f2
oguzhanzeybek <darkstroge@gmail.com>
2022-12-18 05:54:22 +0300

    screen Layout

--- instagram_clone/lib/main.dart
+++ instagram_clone/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
+import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
+import 'package:instagram_clone/responsive/web_screen_layout.dart';
 import 'package:instagram_clone/utils/colors.dart';
 
 void main() {
@@ -8,23 +11,19 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'instagram',
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: mobileBackgroundColor,
-      ),
-      
-      
-        
-      
-      home: Scaffold(body: Text("daha iyisi için şimdi"),)
-      
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'instagram',
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: mobileBackgroundColor,
+        ),
+        home: Scaffold(
+          body: const ReponsiveLayout(
+            webScreenLayout: WebScreenLayout(),
+            mobileScreenLayout: MobilScreenLayout(),
+          ),
+        ));
   }
 }
-
-
